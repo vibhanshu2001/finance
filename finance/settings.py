d157df7254e05b52f00730a18d071090121ce647
Charu1611 <bansalc981@gmail.com>
2022-06-27 19:30:37 +0530

    update

--- finance/settings.py
+++ finance/settings.py
@@ -128,6 +128,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 
 # payment gateway credentials

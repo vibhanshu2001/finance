1ad47e6617d2a9d8d4f4bbf118eb7d5a50dd4f28
Charu1611 <bansalc981@gmail.com>
2022-07-08 18:17:11 +0530

    update

--- finance/settings.py
+++ finance/settings.py
@@ -134,7 +134,7 @@ LOGOUT_URL = 'logout'
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = '/home/ubuntu/django/static/'
+
 # STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ['/home/ubuntu/django/static/']

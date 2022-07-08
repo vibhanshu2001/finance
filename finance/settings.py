bade0f90db79f375958320a4f26991a61013e0a2
Charu1611 <bansalc981@gmail.com>
2022-07-08 18:16:43 +0530

    update

--- finance/settings.py
+++ finance/settings.py
@@ -137,7 +137,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = '/home/ubuntu/django/static/'
 # STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = ['/home/ubuntu/django/static/']
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 

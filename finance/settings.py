49e820b0c4da1d9d5fc114237df1438747669ec8
Charu1611 <bansalc981@gmail.com>
2022-07-08 18:05:13 +0530

    update

--- finance/settings.py
+++ finance/settings.py
@@ -135,16 +135,14 @@ LOGOUT_URL = 'logout'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
-# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 if not DEBUG:
     STATIC_ROOT = ''
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 
 
